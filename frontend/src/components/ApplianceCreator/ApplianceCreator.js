9020b9aba1fc814a541c5d24fa1c3c79f3afce67
alinamiryuk <alinamiryuk@mail.ru>
2020-12-06 15:53:02 +0300

    create form

--- frontend/src/components/ApplianceCreator/ApplianceCreator.js
+++ frontend/src/components/ApplianceCreator/ApplianceCreator.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import { useForm } from '../../hooks/useForm'
 import './ApplianceCreator.css'
 
 export const ApplianceCreator = () => {
+  const [data, setData] = useForm({ model: '', room: '', applianceType: ''})
   return (
-    <>
-      ApplianceCreator
-    </>
+    <div className="applianceCreator">
+      <input value={data.model} onChange={setData} />
+      <select value={data.room} onChange={setData}>
+        <option selected value="bathroom">bathroom</option>
+        <option value="kitchen">kitchen</option>
+      </select>
+      <select value={data.applianceType} onChange={setData}>
+        <option selected value="washingMachine">washing machine</option>
+        <option value="oven">oven</option>
+      </select>
+      <button>ADD</button>
+      {/* <button onClick={e => e.preventDefault() dispatch(fetchCreateAppliance(data))}>ADD</button> */}
+    </div>
   )
 }
